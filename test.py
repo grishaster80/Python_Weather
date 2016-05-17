21c2c6bc380c82441b3d2da2a6e8f99ccdc9d8ba
grishaster80 <grishaster80@gmail.com>
2016-05-17 18:46:48 +0300

    Update test.py

--- test.py
+++ test.py
@@ -3,8 +3,6 @@ a=input('Город?')
 owm = pyowm.OWM('60f45d88e7552897e6188ffbcb1baac3')
 observation = owm.weather_at_place(a)
 w = observation.get_weather()
-w.get_wind()                  
-w.get_humidity()              
 x=w.get_temperature('celsius')
 print('Там ',x['temp'],' градусов')
 
