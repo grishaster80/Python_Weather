6857373d12d9ee43b02dbc713af0977299322528
grishaster80 <grishaster80@gmail.com>
2016-05-17 18:43:35 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,13 @@
+import pyowm
+from gingerit.gingerit import GingerIt
+a=input('Город?')
+owm = pyowm.OWM('60f45d88e7552897e6188ffbcb1baac3')
+observation = owm.weather_at_place(a)
+w = observation.get_weather()
+w.get_wind()                  
+w.get_humidity()              
+x=w.get_temperature('celsius')
+print('Там ',x['temp'],' градусов')
+
+
+
