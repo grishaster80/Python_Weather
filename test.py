a6b886abd32c935e19ee3a486920bf8d4ca1d2b2
grishaster80 <grishaster80@gmail.com>
2017-10-21 17:12:38 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import pyowm
-a=input('Город?')
+a=input('City?')
 owm = pyowm.OWM('60f45d88e7552897e6188ffbcb1baac3')
 observation = owm.weather_at_place(a)
 w = observation.get_weather()
 x=w.get_temperature('celsius')
-print('Там ',x['temp'],' градусов')
+print('There is ',x['temp'],' degrees in the ','a')
